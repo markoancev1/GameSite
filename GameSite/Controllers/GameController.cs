f6a2f883025f435f5befe83119d9efc10e72cbe7
Marko Ancev <ancev.marko@yahoo.com>
2020-08-26 11:10:22 +0200

    01.Editing the Navbar + Creating Controllers+Views

---
+++ GameSite/Controllers/GameController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using GameSite.Data;
+using GameSite.Data.Entities;
+using GameSite.Models;
+using GameSite.Repository;
+using GameSite.Repository.Interface;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Converters;
+
+namespace GameSite.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IGenreRepository _genreRepository;
+        private readonly DataContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public GameController(IGameRepository gameRepository,IGenreRepository genreRepository,DataContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _gameRepository = gameRepository;
+            _genreRepository = genreRepository;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var gameList = _gameRepository.GetAllGames();
+            return View(gameList);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var Genres = _genreRepository.GetAllGenres();
+
+            GameViewModel GameVM = new GameViewModel();
+            GameVM.Genres = GetSelelectListItemsGenres(Genres);
+
+            return View(GameVM);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(GameViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+
+                Game game = new Game
+                {
+                    GameName = model.GameName,
+                    GameCreator = model.GameCreator,
+                    Price = model.Price,
+                    Description = model.Description,
+                    IsOnSale = model.IsOnSale,
+                    IsInStock = model.IsInStock,
+                    GenreId = model.GenreId,
+                    GenreName = model.GenreName,
+                    ImageUrl = uniqueFileName
+                };
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var game = _gameRepository.GetGameByID(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+
+            Game game = _gameRepository.GetGameByID(id);
+
+
+                return View(game);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            
+                
+            _gameRepository.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+        private string UploadedFile(GameViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ImageUrl!= null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageUrl.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ImageUrl.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
+
+        private IEnumerable<SelectListItem> GetSelelectListItemsGenres(IEnumerable<Genre> genres)
+        {
+            var selectList = new List<SelectListItem>();
+            selectList.Add(new SelectListItem
+            {
+                Value = "0",
+                Text = "Select genre..."
+            });
+            foreach (var element in genres)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element.GenreId.ToString(),
+                    Text = element.GenreName
+                });
+            }
+            return selectList;
+        }
+
+    }
+}
---
+++ GameSite/Controllers/GenreController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameSite.Data.Entities;
+using GameSite.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameSite.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreRepository _genreRepository;
+
+        public GenreController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+        public IActionResult Index()
+        {
+            var genreList = _genreRepository.GetAllGenres();
+            return View(genreList);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _genreRepository.Add(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var genre = _genreRepository.GetGenreById(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Genre genre = _genreRepository.GetGenreById(id);
+
+            return View(genre);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                _genreRepository.Delete(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
